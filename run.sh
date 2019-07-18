41474beebfcefc8a6518a4c050ab965d4552eea3
Ubuntu <ubuntu@ip-172-31-71-64.ec2.internal>
2019-07-18 15:09:42 +0000

    Changed script input format to ./run.sh /some/dir file_name --args

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 IMAGE_NAME='xtract_netcdf_image'
 
 args_array=("$@")
-DIRECTORY=("${args_array[@]:0:1}")   
-CMD_ARGS=("${args_array[@]:1}")
+DIRECTORY=("${args_array[@]:0:1}")
+FILE_NAME=("${args_array[@]:1:1}")
+CMD_ARGS=("${args_array[@]:2}")
 
-docker run -it -v "${DIRECTORY[@]}":/"${DIRECTORY[@]}" $IMAGE_NAME "${CMD_ARGS[@]}"
+docker run -v $DIRECTORY:/$DIRECTORY $IMAGE_NAME --path /$DIRECTORY/$FILE_NAME "${CMD_ARGS[@]}"
 
